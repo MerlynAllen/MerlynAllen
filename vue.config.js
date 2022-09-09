b4f432dcbfd2257f9138fabfe41f63e7b78c3b28
Merlyn Allen <me@xiaorui.li>
2022-09-09 20:45:59 +0800

    Fix: changed publicPath from absolute path to relative path to satisfy no-root site situations.

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
+    publicPath: "./",
     pages: {
         index: {
             entry: 'src/main.js',
